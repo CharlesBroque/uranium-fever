6da874aae928d36a7cbc3aabcaf3a8458e81dafe
CharlesBroque <charlesbroque@outlook.com>
2017-03-03 12:27:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+var landExplored = 0;
+var exploreCap = 1;
+var landps = 0;
 
+venture(number, whodunit){
+  if (whodunit = "player") {
+    landExplored = landExplored + exploreCap;
+  };
+  else landExplored = landExplored + landps
+};
